ba928cd96155061199277eb3b45c94c0a77fb3b9
hardiksingla <07hardiksingla@gmail.com>
2023-10-08 13:15:43 +0530

    feat:integrate frontend and backend

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+        def recognize_sign_language(self):
+            # function for sign language recognition
+            recognized_text = "This is a placeholder for recognized sign language."
+            self.text_output.append(recognized_text)
