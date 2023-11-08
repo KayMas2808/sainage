e9b4b987f9dbd16c50b607fbf28489c261a6140d
KayMas2808 <sammyakjain2808@gmail.com>
2023-11-08 13:15:35 +0530

    added edit password

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 import sys
 import sqlite3
 import subprocess
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox, QDialog
 from PyQt5.QtCore import Qt
 
 class LoginWindow(QWidget):
@@ -18,6 +18,7 @@ class LoginWindow(QWidget):
         self.password_input.setEchoMode(QLineEdit.Password)
         self.login_button = QPushButton("Login")
         self.signup_button = QPushButton("Sign Up")
+        self.edit_password_button = QPushButton("Edit Password")  # New button for editing password
 
         # Apply QSS to style the widgets
         self.setStyleSheet("""
@@ -55,6 +56,7 @@ class LoginWindow(QWidget):
         self.form_layout.addWidget(self.password_input)
         self.button_layout.addWidget(self.login_button)
         self.button_layout.addWidget(self.signup_button)
+        self.button_layout.addWidget(self.edit_password_button)  # Add the new button
         self.main_layout.addLayout(self.form_layout)
         self.main_layout.addLayout(self.button_layout)
 
@@ -64,6 +66,7 @@ class LoginWindow(QWidget):
         # Connect signals and slots
         self.login_button.clicked.connect(self.login)
         self.signup_button.clicked.connect(self.signup)
+        self.edit_password_button.clicked.connect(self.edit_password)  # Connect the new button
 
         # Create database connection
         self.conn = sqlite3.connect("users.db")
@@ -97,6 +100,44 @@ class LoginWindow(QWidget):
             self.cursor.execute("INSERT INTO users VALUES (?, ?)", (username, password))
             self.conn.commit()
             QMessageBox.information(self, "Sign Up Successful", "You have successfully signed up.")
+
+    def edit_password(self):
+        username = self.username_input.text()
+        password = self.password_input.text()
+
+        # Check if user exists in the database
+        self.cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+        user = self.cursor.fetchone()
+
+        if user:
+            # Create a dialog to get the new password
+            dialog = QDialog(self)
+            dialog.setWindowTitle("Edit Password")
+            dialog.setFixedSize(300, 150)
+
+            new_password_label = QLabel("New Password:")
+            new_password_input = QLineEdit()
+            new_password_button = QPushButton("Save")
+
+            new_password_layout = QVBoxLayout()
+            new_password_layout.addWidget(new_password_label)
+            new_password_layout.addWidget(new_password_input)
+            new_password_layout.addWidget(new_password_button)
+
+            dialog.setLayout(new_password_layout)
+
+            # Define a function to save the new password
+            def save_new_password():
+                new_password = new_password_input.text()
+                self.cursor.execute("UPDATE users SET password=? WHERE username=?", (new_password, username))
+                self.conn.commit()
+                QMessageBox.information(self, "Password Updated", "Password has been updated.")
+                dialog.accept()
+
+            new_password_button.clicked.connect(save_new_password)
+            dialog.exec_()
+        else:
+            QMessageBox.warning(self, "Edit Password Failed", "Invalid username or password.")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
